3d93a37f12f4814ed6ff588cb404e2e8d5d35ff0
Maxim <polyakovmaxim05@gmail.com>
2023-03-02 16:12:15 -0500

    Routes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,16 +17,12 @@ export class HeroesComponent {
   constructor(private heroService: HeroService, private messageService: MessageService){}
 
   ngOnInit(): void{
-    this.getHeroes()
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
-
-  getHeroes(): void{
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
 }
